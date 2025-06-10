7d0babcd96ff235262b7b91ec2b10b9a08b983d6
Michael Hong <mt.hong26@gmail.com>
2025-06-09 18:41:12 -0700

    Made revisions based on project reviewer's feedback. Also added in new graphs for 4wd analysis

--- app.py
+++ app.py
@@ -4,31 +4,30 @@ import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
 # Fixing data
-df['model_year'] = df['model_year'].astype("Int64")
-df['odometer'] = df['odometer'].astype("Int64")
-# Changing 'is_4wd' column to Integer64 dtype and filled NaN with 0
-df['is_4wd'] = df['is_4wd'].fillna(0).astype("Int64")
-# Converting 'price' and 'days_listed' column to Int64 dtype to keep consistency
-df['price'] = df['price'].astype("Int64")
-df['days_listed'] = df['days_listed'].astype("Int64")
+# Filling in missing values in 'model_year', 'odometer', and 'cylinders' columns with median values relative to vehicle types
+df['model_year'] = df.groupby('type')['model_year'].transform(lambda x: x.fillna(x.median()))
+df['odometer'] = df.groupby('type')['odometer'].transform(lambda x: x.fillna(x.median()))
+df['cylinders'] = df.groupby('type')['cylinders'].transform(lambda x: x.fillna(x.median()))
 
-# Changing columns to category dtype
-df['model'] = df['model'].astype('category')
-df['condition'] = df['condition'].astype('category')
-# Filling NaN values in 'cylinders' with 'unknown'
-df['cylinders'] = df['cylinders'].fillna(0).astype(int)
-df['cylinders'] = df['cylinders'].replace(0, 'unknown').astype('category')
-df['fuel'] = df['fuel'].astype('category')
-df['transmission'] = df['transmission'].astype('category')
-df['type'] = df['type'].astype('category')
-df['paint_color'] = df['paint_color'].astype('category')
-# Filling NaN values in 'paint_color' with 'unknown'
+# Convert multiple columns to int at once 
+int_cols = ['model_year', 'odometer', 'price', 'days_listed', 'cylinders']
+for col in int_cols:
+    df[col] = pd.to_numeric(df[col], errors='coerce').astype('int')
+
+# Filling NaN in is_4wd'with 0 and changing to bool dtype
+df['is_4wd'] = df['is_4wd'].fillna(0).astype("bool")
+
+# Converting mltiple columns to category dtype and filling NaN values in 'paint_color' with 'unknown'
+cat_cols = ['model', 'condition', 'fuel', 'transmission', 'type', 'paint_color']
+for col in cat_cols:
+    df[col] = df[col].astype('category')
 df['paint_color'] = df['paint_color'].cat.add_categories(['unknown']).fillna('unknown')
+
 
 # Converting 'date_posted' column to datetime dtype
 df['date_posted'] = pd.to_datetime(df['date_posted'], errors='coerce')
 # Droping rows with null 'model_year' or 'odometer' values
-df = df.dropna(subset=['model_year', 'odometer'])
+#df = df.dropna(subset=['model_year', 'odometer'])
 
 # Enriching data
 # Extracting the make from the 'model' column and assigning them to its own 'make' column
@@ -42,41 +41,53 @@ df = df[(df['price'] >= 1000) & (df['price'] <= 200000)]
 
 
 
-
 st.header("Vehicle Listings Analysis")
+st.write("Use the checkboxes below to include outliers in the analysis.")
 # Creating a new DF with 'other' vehicle type removed,
 # and only including vehicles priced up to $75,000, model year 1980 and newer and miles 400,000 and under
-df_75k = df
-# df_75k = df_75k[df_75k['price'] <= 75000]
-# df_75k = df_75k[df_75k['model_year'] >= 1980]
-#df_75k = df_75k[df_75k['odometer'] <= 400000]
-# Dropping 'other' vehicle type
-df_75k = df_75k[df_75k['type'] != 'other']
+filtered_df = df.copy()
+filtered_df = filtered_df[filtered_df['type'] != 'other']
+
+# Create columns for a cleaner layout
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if not st.checkbox("Include vehicles > $75,000", key='expensive'):
+        filtered_df = filtered_df[filtered_df['price'] <= 75000]
+
+with col2:
+    if not st.checkbox("Include vehicles older than 1980", key='old'):
+        filtered_df = filtered_df[filtered_df['model_year'] >= 1980]
+
+with col3:
+    if not st.checkbox("Include vehicles > 400k miles", key='high_miles'):
+        filtered_df = filtered_df[filtered_df['odometer'] <= 400000]
 
 st.write("Initial review of the dataset has discovered several outliers for vehicle listings priced above $75,000, model years older than 1980, and odometer readings over 400k miles.")
 st.write("These findings are consistent throughout different vehicle types and makes, allowing the analysis to safely exclude these outliers.")
 
-show_expensive_cars = st.checkbox("Include vehicles above $75,000")
-if show_expensive_cars:
-    df_75k = df_75k
-else:
-    df_75k = df_75k[df_75k['price'] <= 75000]
+# Old Code -- IGNORE
+#show_expensive_cars = st.checkbox("Include vehicles above $75,000")
+#if show_expensive_cars:
+    #filtered_df = filtered_df
+#else:
+    #filtered_df = filtered_df[filtered_df['price'] <= 75000]
 
-show_old_cars = st.checkbox("Include vehicles older than 1980")
-if show_old_cars:
-    df_75k = df_75k
-else:
-    df_75k = df_75k[df_75k['model_year'] >= 1980]
+#show_old_cars = st.checkbox("Include vehicles older than 1980")
+#if show_old_cars:
+    #filtered_df = filtered_df
+#else:
+    #filtered_df = filtered_df[filtered_df['model_year'] >= 1980]
 
-show_high_miles = st.checkbox("Include vehicles with more than 400,000 miles")
-if show_high_miles:
-    df_75k = df_75k
-else:
-    df_75k = df_75k[df_75k['odometer'] <= 400000]
+#show_high_miles = st.checkbox("Include vehicles with more than 400,000 miles")
+#if show_high_miles:
+    #filtered_df = filtered_df
+#else:
+    #filtered_df = filtered_df[filtered_df['odometer'] <= 400000]
 
 # Visualizing the relationship between miles, price and model year
 vehicles_filtered_price_v_miles_scat = px.scatter(
-    df_75k,
+    filtered_df,
     x='odometer',
     y='price',
     labels={'odometer': "Miles", 'price': "Price ($)"},
@@ -86,7 +97,7 @@ st.plotly_chart(vehicles_filtered_price_v_miles_scat)
 st.write("Miles have a negative correlation with price, indicating that as the odometer reading increases, the price tends to decrease.")
 
 vehicles_75k_price_v_year = px.scatter(
-    df_75k,
+    filtered_df,
     x='model_year',
     y='price',
     labels={'model_year': "Model Year", 'price': "Price ($)"},
@@ -97,7 +108,7 @@ st.write("Model year has a positive correlation with price, indicating that newe
 
 # Showing the distribution of vehicle prices for vehicles $75k and under
 vehicle_prices_hist = px.histogram(
-    df_75k,
+    filtered_df,
     x='price',
     nbins=40,
     labels={'price': "Price ($)"},
@@ -106,7 +117,7 @@ vehicle_prices_hist = px.histogram(
 st.plotly_chart(vehicle_prices_hist)
 
 # Grouping vehicles $75k and under by type and calculating the mean price, odometer reading, and days listed
-vehicles_by_type_75k = df_75k.groupby('type', observed=True).agg(
+vehicles_by_type_75k = filtered_df.groupby('type', observed=True).agg(
     count=('type', 'size'),
     avg_price=('price', 'mean'),
     avg_odometer=('odometer', 'mean'),
@@ -124,7 +135,7 @@ vehicle_by_type_75k_bar = px.bar(
 )
 st.plotly_chart(vehicle_by_type_75k_bar)
 
-top4_most_listed_type_75k = df_75k['type'].value_counts().head(4)
+top4_most_listed_type_75k = filtered_df['type'].value_counts().head(4)
 st.write("Top 4 most listed vehicle types ($75k and under):")
 top4_listed_type_df = top4_most_listed_type_75k.to_frame() \
     .reset_index() \
@@ -132,7 +143,7 @@ top4_listed_type_df = top4_most_listed_type_75k.to_frame() \
     .to_html(index=False)
 st.write(top4_listed_type_df, unsafe_allow_html=True)
 
-top4_least_listed_type_75k = df_75k['type'].value_counts()
+top4_least_listed_type_75k = filtered_df['type'].value_counts()
 top4_least_listed_type_75k = top4_least_listed_type_75k[top4_least_listed_type_75k.index != 'other']
 top4_least_listed_type_75k = top4_least_listed_type_75k.tail(4).sort_values(ascending=True)
 st.write("Top 4 least listed vehicle types ($75k and under):")
@@ -143,7 +154,7 @@ bott4_listed_type = top4_least_listed_type_75k.to_frame() \
 st.write(bott4_listed_type, unsafe_allow_html=True)
 
 vehicle_prices_by_type_box = px.box(
-    df_75k,
+    filtered_df,
     x='type',
     y='price',
     labels={'type': "Vehicle Type", 'price': "Price ($)"},
@@ -169,7 +180,7 @@ top4_least_expensive_type_75k_df = top4_least_expensive_type_75k[['type', 'avg_p
 st.write(top4_least_expensive_type_75k_df, unsafe_allow_html=True)
 
 # Grouping vehicles $75k and under by make and calculating the mean price, odometer reading, and days listed
-vehicles_by_make_75k = df_75k.groupby('make', observed=True).agg(
+vehicles_by_make_75k = filtered_df.groupby('make', observed=True).agg(
     count=('make', 'size'),
     avg_price=('price', 'mean'),
     avg_odometer=('odometer', 'mean'),
@@ -187,7 +198,7 @@ vehicle_by_make_75k_bar = px.bar(
 )
 st.plotly_chart(vehicle_by_make_75k_bar)
 
-top4_most_listed_make_75k = df_75k['make'].value_counts().head(4)
+top4_most_listed_make_75k = filtered_df['make'].value_counts().head(4)
 st.write("Top 4 most listed vehicle makes ($75k and under):")
 top4_listed_make_df = top4_most_listed_make_75k.to_frame() \
     .reset_index() \
@@ -195,7 +206,7 @@ top4_listed_make_df = top4_most_listed_make_75k.to_frame() \
     .to_html(index=False)
 st.write(top4_listed_make_df, unsafe_allow_html=True)
 
-top4_least_listed_make_75k = df_75k['make'].value_counts()
+top4_least_listed_make_75k = filtered_df['make'].value_counts()
 top4_least_listed_make_75k = top4_least_listed_make_75k[top4_least_listed_make_75k.index != 'other']
 top4_least_listed_make_75k = top4_least_listed_make_75k.tail(4).sort_values(ascending=True)
 st.write("Top 4 least listed vehicle makes ($75k and under):")
@@ -206,7 +217,7 @@ bott4_listed_make = top4_least_listed_make_75k.to_frame() \
 st.write(bott4_listed_make, unsafe_allow_html=True)
 
 vehicle_prices_by_make_box = px.box(
-    df_75k,
+    filtered_df,
     x='make',
     y='price',
     labels={'make': "Make", 'price': "Price ($)"},
@@ -232,7 +243,7 @@ top4_least_expensive_make_75k_df = top4_least_expensive_make_75k[['make', 'avg_p
 st.write(top4_least_expensive_make_75k_df, unsafe_allow_html=True)
 
 # Grouping vehicles further by make and type
-vehicles_by_maketype_75k = df_75k.groupby(['make', 'type'], observed=True).agg(
+vehicles_by_maketype_75k = filtered_df.groupby(['make', 'type'], observed=True).agg(
     count=('make', 'size'),
     avg_price=('price', 'mean'),
     avg_odometer=('odometer', 'mean'),
@@ -252,3 +263,47 @@ vehicles_by_make_type = px.bar(
     title='Vehicle Listings by Make and Type ($75k and under)'
 )
 st.plotly_chart(vehicles_by_make_type)
+
+# Grouping by 'type' and 'is_4wd', then count
+vehicles_4wd_type_split = filtered_df.groupby(['type', 'is_4wd'], observed=True).size().reset_index(name='count')
+
+# Map True/False to readable labels
+vehicles_4wd_type_split['4WD'] = vehicles_4wd_type_split['is_4wd'].map({True: '4WD', False: 'Not 4WD'})
+
+vehicle_types_4wd_split_bar = px.bar(
+    vehicles_4wd_type_split,
+    x='type',
+    y='count',
+    color='4WD',  # use the new column for legend
+    barmode='stack',
+    labels={'type': "Vehicle Type", 'count': "Listings", '4WD': ""},
+    title='Distribution of 4WD Vehicles by Type ($75k and under)'
+)
+st.plotly_chart(vehicle_types_4wd_split_bar)
+
+# Grouping by 'make' and 'is_4wd', then count
+vehicles_4wd_make_split = filtered_df.groupby(['make', 'is_4wd'], observed=True).size().reset_index(name='count')
+
+# Map True/False to readable labels
+vehicles_4wd_make_split['4WD'] = vehicles_4wd_make_split['is_4wd'].map({True: '4WD', False: 'Not 4WD'})
+
+vehicle_makes_4wd_split_bar = px.bar(
+    vehicles_4wd_make_split,
+    x='make',
+    y='count',
+    color='4WD',  # use the new column for legend
+    barmode='stack',
+    labels={'make': "Vehicle Make", 'count': "Listings", '4WD': ""},
+    title='Distribution of 4WD Vehicles by Make ($75k and under)'
+)
+st.plotly_chart(vehicle_makes_4wd_split_bar)
+
+st.subheader("Exploratory Data Analysis Summary:")
+st.write(
+    "- The dataset contains vehicle listings with details including price, model year, make, model, etc.\n"
+    "- Data cleaning involved filling missing values, converting data types, and removing outliers.\n"
+    "- The outliers removed were vehicles priced over $75,000, older than year 1980 and odometer readings over 400,000 miles.\n"
+    "- Relationship between vehicle attributes are as expected, where price and miles shows a negative correlation, while price and model year shows a positive correlation.\n"
+    "- Majority of vehicles are automatic transmission, with gas fuel being the most common type.\n"
+    "- The dataset of vehicles having AWD is split nearly 50/50, but the split of 4WD vehicles by type and make varies significantly.\n"
+)
